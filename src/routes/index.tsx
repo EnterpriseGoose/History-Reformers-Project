5faacd623a058029621cdc41ee2452cf05bf47ed
null <greatguydev@gmail.com>
2022-03-29 21:21:10 +0000

    Create 15 files

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Component, createSignal, onMount } from "solid-js";
+
+import styles from "./index.module.scss";
+
+const Index: Component = () => {
+  return <div class={styles.main}>a</div>;
+};
+
+export default Index;
